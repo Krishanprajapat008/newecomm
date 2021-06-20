ac1314f7a2addd054a9ae4494d1f99f4cfdd3991
krish <Kprajapat008@gmail.com>
2021-06-20 12:28:37 +0530

    seeders added

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -32,8 +32,13 @@
       
     </li>
 
+    <form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-2" type="text" placeholder="Search">
+    <button class="btn btn-success" type="submit">Submit</button>
+  </form>
+
     
-  <li class="nav-item ml-auto">
+  <li class="nav-item mr">
       <a class="nav-link" href="#">cart(0)</a>
     </li>
     
